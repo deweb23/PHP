c21574b9ab6b6f5bff469e5ecdf6a21b422247db
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2017-04-08 11:31:06 +0300

    Hello world

---
+++ lesson1/index.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * PHP config:
+ * 1) php.ini file
+ * 2) server configuration - nginx, apache etc.
+ * 3) php_set() function
+ *
+ * PHPStorm hot keys
+ * Double shift
+ * Ctrl + Shift + N - search files
+ * Ctrl + Shift + Alt + N - search function by name
+ * Ctrl + Shift + F - search code
+ *
+ * @author Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
+ */
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP</title>
+</head>
+<body>
+    <h1>
+        <?php echo 'Hello, World!' ?>
+    </h1>
+</body>
+</html>
