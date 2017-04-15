616717d1d05e87a708d9813f78bd19f7ea411406
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2017-04-15 13:13:06 +0300

    Functions

---
+++ lesson2/functions.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Functions
+ *
+ * @author Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
+ */
+
+$outer = 'Some text';
+function myFunc()
+{
+//    echo $outer; // variable $outer not accessible in function
+}
+
+myFunc();
