c8380a6e261387b3a54ec539337e33b7a6080ac0
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2017-04-22 11:07:41 +0300

    Request

---
+++ lesson3/index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Request
+ *
+ * @author Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
+ */
+
+var_dump($_GET);
+var_dump($_POST);
+var_dump($_REQUEST);
+?>
+<form action="" method="get">
+    <input type="text" name="login">
+    <input type="text" name="password">
+    <button type="submit">Submit</button>
+</form>
